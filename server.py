704c2b16dcb9d9ad502700339307af0476aee03d
guoyx <guoyx@rgbvr.com>
2017-02-18 17:11:58 +0800

    fix code

--- server.py
+++ server.py
@@ -3,15 +3,15 @@ import os   #Python的标准库中的os模块包含普遍的操作系统功能
 import re   #引入正则表达式对象  
 import urllib   #用于对URL进行编解码  
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler  #导入HTTP处理相关的模块  
-from SocketServer import TCPServer, StreamRequestHandler  
+import random, string
   
 #自定义处理程序，用于处理HTTP请求  
 class MainHTTPHandler(BaseHTTPRequestHandler):  
     #处理GET请求  
-    def do_POST(self):  
+    def do_POST(self):   
         #页面输出模板字符串  
         templateStr ="OK"
-      
+
       
         # 将正则表达式编译成Pattern对象  
         pattern1 = re.compile(r'/handler\?cmd=([^\&]+)$') 
@@ -21,56 +21,65 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
         match2 = pattern2.match(self.path);
         cmd = ''  
         url = ''
-        inputFilename="input.png";
-        outputFilename="output.png";
-        if match1 or match2:  
-            # 使用Match获得分组信息  
-            if match1:
-                cmd = urllib.unquote(match1.group(1))
-            if match2:
-                cmd = urllib.unquote(match2.group(1))
-                url = urllib.unquote(match2.group(2))
-            print("cmd:"+cmd+",url:"+url);
-            
-            # 把url或是body中的图片数据保存到本地。
-            if url!='':
-                urllib.urlretrieve(url, inputFilename);  
-            else :
-                fd = open(inputFilename, 'wb') 
-                content_len = int(self.headers.getheader('content-length', 0))
-                post_body = self.rfile.read(content_len) 
-                try:
-                    fd.write(post_body)  
-                finally:
-                    fd.close()  
-            if os.path.exists(inputFilename):
-                if(os.path.exists(outputFilename)):
-                    os.remove(outputFilename);
-                os.system("pngquant "+inputFilename+" -o "+outputFilename)
+        inputFilename="input"+random_str()+".png";
+        outputFilename="output"+random_str()+".png";
+        try:
+            if match1 or match2:  
+                # 使用Match获得分组信息  
+                if match1:
+                    cmd = urllib.unquote(match1.group(1))
+                if match2:
+                    cmd = urllib.unquote(match2.group(1))
+                    url = urllib.unquote(match2.group(2))
                 
+                # 把url或是body中的图片数据保存到本地。
+                if url!='':
+                    urllib.urlretrieve(url, inputFilename);  
+                else :
+                    fd = open(inputFilename, 'wb') 
+                    content_len = int(self.headers.getheader('content-length', 0))
+                    post_body = self.rfile.read(content_len) 
+                    try:
+                        fd.write(post_body)  
+                    finally:
+                        fd.close()  
+                if os.path.exists(inputFilename):
+                    if(os.path.exists(outputFilename)):
+                        os.remove(outputFilename);
+                    os.system("pngquant "+inputFilename+" -o "+outputFilename)
+                    
+                    self.protocal_version = 'HTTP/1.1'  #设置协议版本  
+                    self.send_response(200) #设置响应状态码  
+                    self.end_headers()
+                    if(os.path.getsize(inputFilename)>os.path.getsize(outputFilename)):
+                        fout=open(outputFilename,'rb');
+                    else:
+                        fout=open(inputFilename,'rb');
+                    try:
+                        self.wfile = self.connection.makefile('wb', self.wbufsize)
+                        self.wfile.write(fout.read())   #输出响应内容
+                    finally:
+                        fout.close(); 
+                else :
+                    self.protocal_version = 'HTTP/1.1'  #设置协议版本  
+                    self.send_response(400) #设置响应状态码  
+            else:
                 self.protocal_version = 'HTTP/1.1'  #设置协议版本  
-                self.send_response(200) #设置响应状态码  
-                self.end_headers()
-                if(os.path.getsize(inputFilename)>os.path.getsize(outputFilename)):
-                    fout=open(outputFilename,'rb');
-                else:
-                    fout=open(inputFilename,'rb');
-                try:
-                    self.wfile = self.connection.makefile('wb', self.wbufsize)
-                    self.wfile.write(fout.read())   #输出响应内容
-                finally:
-                    fout.close(); 
-            else :
-                self.protocal_version = 'HTTP/1.1'  #设置协议版本  
-                self.send_response(400) #设置响应状态码  
-        else:
-            self.protocal_version = 'HTTP/1.1'  #设置协议版本  
-            self.send_response(404) #设置响应状态码  
-        
+                self.send_response(404) #设置响应状态码  
+        finally:
+            if(os.path.exists(inputFilename)):
+                os.remove(inputFilename);
+            if(os.path.exists(outputFilename)):
+                os.remove(outputFilename);        
 
 
 
 
+ 
+def random_str(randomlength=8):
+    a = list(string.ascii_letters)
+    random.shuffle(a)
+    return ''.join(a[:randomlength])
 
 #启动服务函数  
 def start_server(port):  
